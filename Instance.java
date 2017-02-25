83f0b1fc646ce11dba3668380eeb1e7a1ebf87d7
tzrtzr000 <tzrtzr000@gmail.com>
2017-02-24 22:39:45 -0600

    Intance.java completed

--- Instance.java
+++ Instance.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * This is the class for each image instance
  */
 
@@ -10,12 +10,12 @@ import java.awt.image.DataBufferByte;
 
 public class Instance {
 	// store the bufferedImage
-    private BufferedImage image;
+	private BufferedImage image;
 	private String label;
 	private int width, height;
 	// separate rgb channels
 	private int[][] red_channel, green_channel, blue_channel, gray_image;
-    
+
 	// Constructor
 	// given the bufferedimage and its class label
 	// get the 
@@ -24,13 +24,13 @@ public class Instance {
 		this.label = label;
 		width = image.getWidth();
 		height = image.getHeight();
-		
+
 		// get separate rgb channels
 		red_channel = new int[height][width];
 		green_channel = new int[height][width];
 		blue_channel = new int[height][width];
 		gray_image = new int[height][width];
-		
+
 		for(int row = 0; row < height; ++row) {
 			for(int col = 0; col < width; ++col) {
 				Color c = new Color(image.getRGB(col, row));
@@ -40,17 +40,17 @@ public class Instance {
 			}
 		}
 	}
-	
+
 	// get separate red channel image
 	public int[][] getRedChannel() {
 		return red_channel;
 	}
-	
+
 	// get separate green channel image
 	public int[][] getGreenChannel() {
 		return green_channel;
 	}
-	
+
 	// get separate blue channel image
 	public int[][] getBlueChannel() {
 		return blue_channel;
@@ -61,7 +61,7 @@ public class Instance {
 		// Gray filter
 		BufferedImage grayImage = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
 		byte[] dstBuff = ((DataBufferByte) grayImage.getRaster().getDataBuffer()).getData();
-		
+
 		for(int row = 0; row < height; ++row) {
 			for(int col = 0; col < width; ++col) {
 				gray_image[row][col] = dstBuff[col + row * width] & 0xFF;
@@ -69,15 +69,15 @@ public class Instance {
 		}
 		return gray_image;
 	}
-	
+
 	public int getWidth() {
 		return width;
 	}
-	
+
 	public int getHeight() {
 		return height;
 	}
-	
+
 	public String getLabel() {
 		return label;
 	}
